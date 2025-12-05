8e9976da2e9c0babe0aee950bcc4898b8ed757ee
Nilesh <nileshraj18a1@gmail.com>
2025-12-05 09:38:54 +0530

    Restore pandas

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-# import pandas as pd
+import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import os
